879bbc21f39f3e888fe599a363837580cb6fc5f7
alxxyz <75482780+alxxyz@users.noreply.github.com>
2022-10-12 23:43:43 +0300

    Value

--- src/main/java/com/codingsnack/demo/HomeController.java
+++ src/main/java/com/codingsnack/demo/HomeController.java
@@ -2,6 +2,7 @@ package com.codingsnack.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HomeController {
